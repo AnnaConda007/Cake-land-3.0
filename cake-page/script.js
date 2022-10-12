48f1a9a9dc3a796648093061ddae14c2a0110d3e
AnnaConda007 <annahrulkova@yandex.ru>
2022-10-12 22:31:39 +0300

    каталог товаров выводится через ajax

--- cake-page/script.js
+++ cake-page/script.js
@@ -28,9 +28,42 @@ wrap.addEventListener("click", function(e){
     if(targetElement.classList.contains("add-product")) {
 
 
-
-
-
 alert("vbn")
 
 }})
+
+
+
+
+const reguest="http://myjson.dit.upm.es/api/bins/cc08"
+
+const xhr = new XMLHttpRequest()
+let list = document.querySelector(".catalog-box")
+xhr.open("GET", reguest)
+
+xhr.responseType="json"
+ 
+xhr.onload = function() {
+   let response=xhr.response
+   for(let key in response){
+       list.innerHTML+=`
+
+       <div class="product-item" data-id="${response[key].id}">
+       <div class="img-wrap">
+       <img class="product-photo" src="${response[key].img}" alt="foto">
+       </div>
+       <div class="info-about-product">
+           <h3 class="product-name">${response[key].name}</h3>
+             <span class="price">₽ ${response[key].price}</span>
+             <button  class="add-product" >добавить</button>
+   </div>
+   </div>` 
+   }
+ }
+xhr.send()
+
+
+
+
+
+
